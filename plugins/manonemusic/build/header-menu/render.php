63d93c84188fa72f3f57776ff16cce1b75b28471
juliscapucin <jscapucin@gmail.com>
2024-07-08 22:09:46 +0200

    improve header accessibility

--- plugins/manonemusic/build/header-menu/render.php
+++ plugins/manonemusic/build/header-menu/render.php
@@ -15,8 +15,8 @@ $links = $attributes['linkList'];
 
 if (count($links) > 0) {
    echo '
-   <nav class="relative w-1/2">
-      <div class="lg:hidden fixed top-2 right-0 h-8 z-menuButtons">
+   <nav class="relative w-1/2" aria-label="Main Navigation">
+      <div class="lg:hidden fixed top-2 right-0 h-8 z-menuButtons" role="menubar">
          <button class="absolute w-12 h-12 right-4 md:right-16 flex justify-end items-center" js-hook-burger-button>
             <div class="relative w-full" js-hook-button-lines-container>
                <div class="w-full h-[1px] bg-secondary" js-hook-button-line></div>
@@ -26,7 +26,7 @@ if (count($links) > 0) {
       </div>
       <ul class="fixed inset-0 lg:relative flex flex-col justify-center items-center lg:flex-row lg:justify-end gap-8 z-30 pointer-events-none" js-hook-link-list>';
    foreach ($links as $link) {
-      echo '<li class="pointer-events-auto' . ($link['slug'] == '/home' ? ' lg:hidden ' : '') . '"><a href="' . $link['slug'] . '" class="underlined-link text-titleLarge lg:text-bodyMedium lg:underlined-link uppercase">' . $link['label'] . '</a></li>';
+      echo '<li role="none" class="pointer-events-auto' . ($link['slug'] == '/home' ? ' lg:hidden ' : '') . '"><a role="menuitem" href="' . $link['slug'] . '" class="underlined-link text-titleLarge lg:text-bodyMedium lg:underlined-link uppercase">' . $link['label'] . '</a></li>';
    }
    echo '
       </ul>
--- themes/manonemusic/patterns/header.php
+++ themes/manonemusic/patterns/header.php
@@ -9,18 +9,13 @@
 
 $items = array('Films', 'Commercials', 'Projects', 'Releases', 'About', 'Contact');
 
-// Define the base URL for the links
 $base_url = "/";
 
-// Start the unordered list
 echo '<ul class="flex gap-8">';
 
-// Loop through the array to create list items with links
 foreach ($items as $item) {
-   // Create a slug from the item name for the URL
    $slug = strtolower($item);
    echo '<li><a href="' . $base_url . $slug . '" class="underlined-link uppercase">' . $item . '</a></li>';
 }
 
-// End the unordered list
 echo '</ul>';
